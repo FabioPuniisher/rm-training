79e0e3dae151ee99938bd9eb0f63999c016703da
Fábio Relvas <faabio.relvas@hotmail.com>
2019-03-11 00:37:29 -0300

    Inserindo filtro por gênero e status na busca

--- js/main.js
+++ js/main.js
@@ -1,11 +1,56 @@
 const searchInput = document.getElementById('search');
+const statusSelect = document.getElementById('status');
+const genderSelect = document.getElementById('gender');
 const mainWrapper = document.querySelector('.main-wrapper');
 const errorElement = document.querySelector('.error');
 const spinnerElement = document.querySelector('.spinner');
 const CHARACTERS_COUNT = 493;
 
+const statuses = [
+    {
+        text: 'Todos',
+        value: '',
+    },
+    {
+        text: 'Vivo',
+        value: 'alive',
+    },
+    {
+        text: 'Morto',
+        value: 'dead',
+    },
+    {
+        text: 'Desconhecido',
+        value: 'unknown',
+    },
+];
+
+const genders = [
+    {
+        text: 'Todos',
+        value: '',
+    },
+    {
+        text: 'Feminino',
+        value: 'female',
+    },
+    {
+        text: 'Masculino',
+        value: 'male',
+    },
+    {
+        text: 'Indefinido',
+        value: 'genderless',
+    },
+    {
+        text: 'Desconhecido',
+        value: 'unknown',
+    },
+];
+
 window.onload = function () {
     showRandomCharacters();
+    setupSelectors();
 };
 
 function showRandomCharacters() {
@@ -42,7 +87,8 @@ function showRandomCharacters() {
 
 function findCharacter() {
     const characterName = searchInput.value;
-    const characterStatus = searchInput.value;
+    const characterStatus = statusSelect.value;
+    const characterGender = genderSelect.value;
 
     if (characterName.length < 1) {
         return;
@@ -54,7 +100,15 @@ function findCharacter() {
 
     // https://rickandmortyapi.com/api/character/?name={name}
 
-    fetch(`https://rickandmortyapi.com/api/character/?name=${characterName}`)
+    const params = {
+        name: characterName,
+        status: characterStatus,
+        gender: characterGender,
+        // species: characterSpecies,
+        // type
+    };
+
+    fetch(`https://rickandmortyapi.com/api/character/?${serialize(params)}`)
         .then(function(res) {
             if (!res.ok) {
                 errorElement.removeAttribute('hidden');
@@ -141,9 +195,34 @@ function createFigureElement(character, parentEl) {
     parentEl.appendChild(figureEl);
 }
 
+function setupSelectors() {
+    statuses.forEach(function(status) {
+        let option = document.createElement('option');
+        option.value = status.value;
+        option.textContent = status.text;
+        statusSelect.appendChild(option);
+    });
+
+    genders.forEach(function (gender) {
+        let option = document.createElement('option');
+        option.value = gender.value;
+        option.textContent = gender.text;
+        genderSelect.appendChild(option);
+    });
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+function serialize(obj) {
+    let str = [];
+    for (let p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
 }
