eb23a1edbf06923eccc0206dfd6648f0b506ebe1
Fábio Relvas <faabio.relvas@hotmail.com>
2019-03-14 00:50:30 -0300

    Reorganizando repo e utilizando webpack com loader sass e babel

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            { from: 'public/', to: './' },
+        ]),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+        ]
+    },
+};
